ea2c79cbe2ea07f703324420e9894bf07d3b902e
Brody Fischer <brodyf42@gmail.com>
2021-11-09 19:02:15 -0800

    implementd basic post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,22 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    def create
+        user_email = params[:post][:user_email].strip.downcase
+        user = User.find_by(email: user_email)
+        if !user
+            @post = Post.new
+            render :new and return
+        end
+
+        body = params[:post][:body]
+        @post = Post.new(user_id: user.id, body: body, active: true)
+
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
+    end
 end
