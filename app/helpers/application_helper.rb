3f6b7b8a3b90829100d9790f86f3ef0f3e494655
Brody Fischer <brodyf42@gmail.com>
2021-11-09 23:40:33 -0800

    implemented gravatar identification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_img_url(email)
+        standardized_email = email.strip.downcase
+        gravatar_hash = Digest::MD5.hexdigest standardized_email
+        default_theme = 'retro'
+        gravatar_img_url = "https://www.gravatar.com/avatar/#{gravatar_hash}?d=#{default_theme}"
+    end
 end
