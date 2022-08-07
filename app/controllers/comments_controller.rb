85bfa81098af662b290580a7c2263bbfae848546
Brody Fischer <brodyf42@gmail.com>
2022-08-06 22:17:24 -0700

    Refactored Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,43 @@
 class CommentsController < ApplicationController
-    def create
-        # check that associated post exists
-        post = Post.find(params[:post_id])
-        redirect_to post_path if !post
-        #check that associated user exists
-        user_email = params[:user_email].strip.downcase
-        user = User.find_by(email: user_email)
-        if !user
-            flash[:error] = "You must provide an email address for a user that exists."
-            redirect_to post_path(post.id) and return
-        end
-        # check that body is not empty
-        body = params[:body]
-        if body.empty?
-            flash[:error] = "You cannot submit a comment without a body."
-            redirect_to post_path(post.id) and return
-        end
-        # save comment and reload post thread
-        Comment.create(user_id: user.id, post_id: post.id, body: body)
-        redirect_to post_path(post.id)
-    end
+  before_action :find_post, only: [:create, :destroy]
 
-    def destroy
-        post = Post.find(params[:post_id])
-        redirect_to post_path if !post
-        @comment = Comment.find(params[:comment_id])
-        @comment.destroy
-        redirect_to post_path(post.id)
+  def create
+    Comment.create(post: @post, user: @user, body: @body) if valid_request?
+    redirect_to_post
+  end
+
+  def destroy
+    Comment.find_by(id: params[:comment_id])&.destroy
+    redirect_to_post
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to post_path and return
+  end
+
+  def valid_request?
+    @post.present? && valid_user? && valid_body?
+  end
+
+  def valid_user?
+    @user = User.find_by(email: params[:user_email].strip.downcase)
+    @user.present?.tap do |valid_user|
+      flash[:error] = "You must provide an email address for a user that exists." unless valid_user
     end
+  end
+
+  def valid_body?
+    @body = params[:body]
+    @body.present?.tap do |valid_body|
+      flash[:error] = "You cannot submit a comment without a body." unless valid_body
+    end
+  end
+
+  def redirect_to_post
+    redirect_to post_path(@post.id)
+  end
 end
